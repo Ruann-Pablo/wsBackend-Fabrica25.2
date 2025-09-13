21230bda701a1945ef4fd21665a50dc7931dec73
Ruann Pablo <129422692+Ruann-Pablo@users.noreply.github.com>
2025-09-13 03:24:57 -0300

    :sparkles: feat: Adicionar campo de senha ao UserSerializer e implementar métodos de criação e atualização

--- weather/serializers.py
+++ weather/serializers.py
@@ -2,9 +2,30 @@ from rest_framework import serializers
 from .models import User, City, Forecast, FavoriteCity
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'city']
+        fields = ['id', 'username', 'email', 'city', 'password']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            city=validated_data.get('city', '')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('username', instance.username)
+        instance.city = validated_data.get('city', instance.city)
+        password = validated_data.get('password', None)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:

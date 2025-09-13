e54fb8073b1dae146a9e1b8cc93f62d7d4f5b7b5
Ruann Pablo <129422692+Ruann-Pablo@users.noreply.github.com>
2025-09-13 03:03:23 -0300

    :sparkles: feat: Implementar viewsets para os modelos User, City, Forecast e FavoriteCity

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, City, Forecast, FavoriteCity
+from .serializers import UserSerializer, CitySerializer, ForecastSerializer, FavoriteCitySerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class ForecastViewSet(viewsets.ModelViewSet):
+    queryset = Forecast.objects.all()
+    serializer_class = ForecastSerializer
+
+class FavoriteCityViewSet(viewsets.ModelViewSet):
+    queryset = FavoriteCity.objects.all()
+    serializer_class = FavoriteCitySerializer

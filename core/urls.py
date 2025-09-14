d07c06e65abfdf0d4484055d299f44577515565c
Ruann Pablo <129422692+Ruann-Pablo@users.noreply.github.com>
2025-09-14 07:17:27 -0300

    :recycle: refactor: Remove commented-out code and clean up URL patterns in urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('weather.urls')),
 
-    # URLs para autenticação JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Incluindo as URLs do app front_app
-    ## path('', include('front_app.urls')),
-
 ]
